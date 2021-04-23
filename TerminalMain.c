d02f02516d92d03e9d27396f1e49702de7f20da8
FattyKitty <tony.shumlin@gmail.com>
2021-04-23 21:44:08 +0300

    Added Ctr+C interruprtion

--- TerminalMain.c
+++ TerminalMain.c
@@ -1,12 +1,16 @@
 #include "TerminalHeader.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 int main()
 {
     char *InputLine=NULL;
     char **CommsAndArgs;
     int Executable=1;
+
+    struct sigaction signal;
+
     do
     {
         printf("Type here > ");
@@ -15,6 +19,8 @@ int main()
         CommsAndArgs=ParsingLine(InputLine);
 
         Executable=LaunchProcess(CommsAndArgs);
+
+        sigaction(SIGINT, &signal, NULL);
 
         free(InputLine);
         free(CommsAndArgs);
